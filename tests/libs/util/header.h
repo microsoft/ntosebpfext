ce84e34abb9cde205cfcb775fbf8d6a0a7c9a428
Alan Jowett <alan.jowett@microsoft.com>
2024-03-04 19:49:26 -0800

    Cleanup part 5
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>

--- tests/libs/util/header.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation
-// SPDX-License-Identifier: MIT
-#pragma once
-
-#include <windows.h>
-#include <ctype.h>
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
