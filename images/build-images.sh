bf7abfeaf8718aa70d46e3d2a2616333e1d9d1d1
Alan Jowett <alan.jowett@microsoft.com>
2024-03-04 18:48:14 -0800

    Removal part 1
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>

--- images/build-images.sh
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Microsoft Corporation
-# SPDX-License-Identifier: MIT
-
-# Download and copy release archive to local directory as ./ebpf-for-windows.msi before running this script.
-
-repository=${repository:-"your repository"}
-tag=${tag:-"your tag"}
-
-docker buildx build --platform windows/amd64 --output=type=registry --pull -f Dockerfile.install -t $repository/ebpfwin-install:$tag .
