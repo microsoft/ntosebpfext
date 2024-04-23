1dad54b4661797c2c47ba235a3b035b15f74598f
Alan Jowett <alan.jowett@microsoft.com>
2024-04-22 17:00:32 -0700

    Setup repo
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>

---
+++ scripts/initialize_repo.ps1
@@ -0,0 +1,22 @@
+# Copyright (c) Microsoft Corporation
+# SPDX-License-Identifier: MIT
+
+# Define the commands to run
+$commands = @(
+    "git submodule update --init --recursive",
+    "cmake -G 'Visual Studio 17 2022' -S external\catch2 -B external\catch2\build -DBUILD_TESTING=OFF",
+    "nuget restore ntosebpfext.sln.sln"
+)
+
+# Loop through each command and run them sequentially without opening a new window
+foreach ($command in $commands) {
+    Write-Host ">> Running command: $command"
+    Invoke-Expression -Command $command
+
+    # Check the exit code
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Command failed. Exit code: $LASTEXITCODE"
+        Exit  $LASTEXITCODE
+    }
+}
+Write-Host "All commands succeeded."
