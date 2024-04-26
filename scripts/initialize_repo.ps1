ea06562b6a12531ea3ac405c015d86b18ea5f477
Alan Jowett <alanjo@microsoft.com>
2024-04-26 08:21:46 -0700

    Switch to 0.16.0 (#24)
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>
    Co-authored-by: Alan Jowett <alan.jowett@microsoft.com>

--- scripts/initialize_repo.ps1
+++ scripts/initialize_repo.ps1
@@ -6,7 +6,7 @@ $commands = @(
     "git submodule update --init --recursive",
     "cmake -G 'Visual Studio 17 2022' -S external\catch2 -B external\catch2\build -DBUILD_TESTING=OFF",
     "nuget restore ntosebpfext.sln",
-    "packages\eBPF-for-Windows.0.15.1\build\native\bin\export_program_info.exe"
+    "packages\eBPF-for-Windows.0.16.0\build\native\bin\export_program_info.exe"
 )
 
 # Loop through each command and run them sequentially without opening a new window
