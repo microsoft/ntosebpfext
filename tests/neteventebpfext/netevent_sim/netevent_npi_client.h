46652f019b0a63f5839b5c533cc800b4a7a641eb
Alan Jowett <alan.jowett@microsoft.com>
2024-06-27 10:09:26 -0700

    Fix ODR violation
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>

--- tests/neteventebpfext/netevent_sim/netevent_npi_client.h
+++ tests/neteventebpfext/netevent_sim/netevent_npi_client.h
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: MIT
 
 #pragma once
+#include "ebpf_windows.h"
 
 // Specific Event type to the neteventebpfext extension.
 typedef struct
@@ -12,17 +13,6 @@ typedef struct
                                    ///< the memory range).
 } netevent_event_info_t;
 typedef void (*netevent_push_event)(netevent_event_info_t*);
-
-// Header of an eBPF extension data structure.
-// Every eBPF extension data structure must start with this header.
-// New fields can be added to the end of an eBPF extension data structure
-// without breaking backward compatibility. The version field must be
-// updated only if the new data structure is not backward compatible.
-typedef struct _ebpf_extension_header
-{
-    UINT16 version; ///< Version of the extension data structure.
-    size_t size;    ///< Size of the extension data structure.
-} ebpf_extension_header_t;
 
 // This is the type definition for the eBPF helper function addresses
 // when version is EBPF_HELPER_FUNCTION_ADDRESSES_CURRENT_VERSION.
