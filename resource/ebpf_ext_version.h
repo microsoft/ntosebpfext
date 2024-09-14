7367ca8f610342dfa0f11695d522cdc4aa1d8235
Shankar Seal <74580197+shankarseal@users.noreply.github.com>
2024-09-13 18:11:47 -0700

    version 0.1.0 (#111)

--- resource/ebpf_ext_version.h
+++ resource/ebpf_ext_version.h
@@ -2,8 +2,8 @@
 // SPDX-License-Identifier: MIT
 
 #define EBPF_VERSION_MAJOR 0
-#define EBPF_VERSION_MINOR 0
-#define EBPF_VERSION_REVISION 1
+#define EBPF_VERSION_MINOR 1
+#define EBPF_VERSION_REVISION 0
 
 #define QUOTE(str) #str
 #define EXPAND_AND_QUOTE(str) QUOTE(str)
