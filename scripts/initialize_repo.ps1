ac523893f9443574918ec3824db1e1c12980a314
Venkata Naga Kartik Pidatala <vpidatala@microsoft.com>
2025-04-15 23:02:03 -0700

    ebpf 21

--- scripts/initialize_repo.ps1
+++ scripts/initialize_repo.ps1
@@ -12,7 +12,7 @@ $commands = @(
     "git submodule update --init --recursive",
     "cmake -G 'Visual Studio 17 2022' -S external\catch2 -B external\catch2\build -DBUILD_TESTING=OFF",
     "nuget restore ntosebpfext.sln",
-    "packages\eBPF-for-Windows.0.18.0\build\native\bin\export_program_info.exe"
+    "packages\eBPF-for-Windows.x64.0.21.0\build\native\bin\export_program_info.exe"
 )
 
 # Loop through each command and run them sequentially without opening a new window
