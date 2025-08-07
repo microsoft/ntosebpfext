b664e9fbfb950a1f325edb0e6825a814b97ae42d
Laksh <lakshkotian98@gmail.com>
2025-08-07 15:26:14 -0700

    Add perf array (#242)
    * switched ring_buff to perf_array
    
    * fix version header
    
    * fix version header
    
    * better flag name
    
    * remove redef
    
    * extra space remove
    
    ---------
    
    Co-authored-by: Laksh Kotian <lakshkotian@microsoft.com>

--- tools/netevent_monitor/bpf/netevent_monitor.c
+++ tools/netevent_monitor/bpf/netevent_monitor.c
@@ -9,11 +9,13 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#define EVENT_SIZE_MAX 128
+
 // Ring-buffer for netevent_event_md_t.
 #define EVENTS_MAP_SIZE (512 * 1024)
 struct
 {
-    __uint(type, BPF_MAP_TYPE_RINGBUF);
+    __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
     __uint(max_entries, EVENTS_MAP_SIZE);
 } netevent_events_map SEC(".maps");
 
@@ -27,14 +29,19 @@ int
 NetEventMonitor(netevent_event_md_t* ctx)
 {
     int result = -1;
-    uint32_t event_header = sizeof(netevent_data_header_t) + PKTMON_EVENT_HEADER_LENGTH;
-    uint32_t full_context_size = ctx->data_end - ctx->data_meta;
-    if (ctx != NULL && ctx->data != NULL && ctx->data_end != NULL && ctx->data_meta + event_header == ctx->data) {
-        // Push the event to the netevent_events_map.
-        // TODO: switch to perf_event_output when it is available.
-        // Issue: https://github.com/microsoft/ntosebpfext/issues/204.
-        result = bpf_ringbuf_output(&netevent_events_map, ctx->data_meta, full_context_size, 0);
+
+    uint32_t ctx_len = ctx->data_end - ctx->data_meta;
+    uint32_t data_len = ctx->data_end - ctx->data;
+    uint32_t header_len = sizeof(netevent_data_header_t) + PKTMON_EVENT_HEADER_LENGTH;
+    uint32_t event_len = header_len;
+
+    if (ctx->data_meta + header_len == ctx->data) {
+        if (data_len > EVENT_SIZE_MAX) {
+            data_len = EVENT_SIZE_MAX;
+        }
+        event_len += data_len;
+        return bpf_perf_event_output(ctx, &netevent_events_map, EBPF_MAP_FLAG_CURRENT_CPU, ctx->data_meta, event_len);
     }
 
-    return result;
+    return -1;
 }
