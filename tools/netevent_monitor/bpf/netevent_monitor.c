81507357c1489eb5e7ecfae09ea0fca3b04ec228
Laksh <lakshkotian98@gmail.com>
2025-08-07 14:42:03 -0700

    extra space remove

--- tools/netevent_monitor/bpf/netevent_monitor.c
+++ tools/netevent_monitor/bpf/netevent_monitor.c
@@ -19,7 +19,6 @@ struct
     __uint(max_entries, EVENTS_MAP_SIZE);
 } netevent_events_map SEC(".maps");
 
-
 // The following line is optional, but is used to verify
 // that the NetEventMonitor prototype is correct or the compiler
 // would complain when the function is actually defined below.
